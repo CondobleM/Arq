55b74a4d100873c3a7b403d0ebbaf63e1bdbe4bf
doblem <condobem@gmail.com>
2020-09-07 15:32:14 -0500

    commit

---
+++ assets/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Vamos! debes tener al menos un apodo",
+                    minlength: "Tu nombre debe tener al menos 2 letras"
+                },
+                subject: {
+                    required: "Vamos! debes tener alguna duda",
+                    minlength: "Tu asunto debe contener al menos 4 letras"
+                },
+                number: {
+                    required: "Tienes un número de teléfono?",
+                    minlength: "Tu número debe contener al menos 8 cifras"
+                },
+                email: {
+                    required: "Sin email, no hay mensaje"
+                },
+                message: {
+                    required: "Si ammm debes escribir algo para poder enviarlo",
+                    minlength: "Eso es todo? en serio?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
